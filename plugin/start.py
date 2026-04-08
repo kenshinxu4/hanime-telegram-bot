43d20f55dfb54bec7fb62c4582b01980c9332713
kenshinxu4 <kenshinxu.4@gmail.com>
2026-04-08 11:41:57 +0530

    Update start.py

--- plugin/start.py
+++ plugin/start.py
@@ -2,7 +2,5 @@ from pyrogram import *
 from pyrogram.types import *
 
 def start(client, message):
-    message.reply_text(
-    "**/search <hentai name> to search**\n Powered By @KENSHIN_ANIME", 
-    parse_mode="md"
-)
+    # Sirf text likho, parse_mode likhne ki zaroorat nahi hai
+message.reply_text("**/search <hentai name> to search**\n Powered By @KENSHIN_ANIME")
