5c07f4147c57cc5b67b8d49e78cc9275d319bf2d
kenshinxu4 <kenshinxu.4@gmail.com>
2026-04-08 11:50:19 +0530

    Update info_hentai.py

--- plugin/info_hentai.py
+++ plugin/info_hentai.py
@@ -1,29 +1,35 @@
-from pyrogram import *
-from pyrogram.types import *
+from pyrogram import Client, enums
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
-import os
-import json
 
-
-
-
-def infohentai(client, callback_query):
+async def infohentai(client, callback_query):
     click = callback_query.data
     clickSplit = click.split("_")
     query = clickSplit[1]
+    
     chatid = callback_query.from_user.id
-    messageid = callback_query.message.message_id
+    messageid = callback_query.message.id
+    
     url = f"https://apikatsu.otakatsu.studio/api/hanime/details?id={query}" 
-    result = requests.get(url) 
-    result = result.json()   
-    description = result["description"]
-    name = result["name"]
-    img = result["poster"]
-    view = result["views"]     
-    released_date = result["released_date"]
+    result = requests.get(url).json()
+    
+    name = result.get("name", "N/A")
+    img = result.get("poster", "")
+    view = result.get("views", "0")     
+    released_date = result.get("released_date", "N/A")
+    
     keyb = [
         [InlineKeyboardButton("Download Now", callback_data=f"dlt_{query}")],
         [InlineKeyboardButton("Link", callback_data=f"link_{query}")]
     ]
     repl = InlineKeyboardMarkup(keyb)
-    client.edit_message_text(chat_id=chatid, message_id=messageid, text=f"""**Name:** [{name}]({img})\n**View:** {view}\n**Release Date:** {released_date}""", reply_markup=repl, parse_mode="markdown")
+    
+    text = f"**Name:** [{name}]({img})\n**View:** {view}\n**Release Date:** {released_date}"
+    
+    await client.edit_message_text(
+        chat_id=chatid, 
+        message_id=messageid, 
+        text=text, 
+        reply_markup=repl, 
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
