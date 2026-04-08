c04373fa9bcc52bf9e908a656a17fb3bb0c1afb2
kenshinxu4 <kenshinxu.4@gmail.com>
2026-04-08 11:44:57 +0530

    Update start.py

--- plugin/start.py
+++ plugin/start.py
@@ -2,5 +2,5 @@ from pyrogram import *
 from pyrogram.types import *
 
 def start(client, message):
-    # Sirf text likho, parse_mode likhne ki zaroorat nahi hai
-message.reply_text("**/search <hentai name> to search**\n Powered By @KENSHIN_ANIME")
+    # Ye line function ke andar hai, isliye aage 4 spaces (Tab) zaroori hain
+    message.reply_text("**/search <hentai name> to search**\n Powered By @KENSHIN_ANIME")
