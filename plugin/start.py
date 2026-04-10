c5a8cf66db71db02f138cb74eac2c651a0dd1dc3
kenshinxu4 <kenshinxu.4@gmail.com>
2026-04-10 09:11:57 +0530

    Delete plugin directory

--- plugin/start.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import *
-from pyrogram.types import *
-
-def start(client, message):
-    # Ye line function ke andar hai, isliye aage 4 spaces (Tab) zaroori hain
-    message.reply_text("**/search <hentai name> to search**\n Powered By @KENSHIN_ANIME")
